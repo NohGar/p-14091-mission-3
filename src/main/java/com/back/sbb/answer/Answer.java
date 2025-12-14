07f43321a6999e21b0436930aae5ba5ff39e1239
NohGar <njwwn193@naver.com>
2025-12-14 13:48:58 +0900

    3-11

--- src/main/java/com/back/sbb/answer/Answer.java
+++ src/main/java/com/back/sbb/answer/Answer.java
@@ -6,6 +6,7 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import java.time.LocalDateTime;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -25,4 +26,9 @@ public class Answer {
 
     @ManyToOne
     private SiteUser author;
+
+    private LocalDateTime modifyDate;
+
+    @ManyToMany
+    Set<SiteUser> voter;
 }
